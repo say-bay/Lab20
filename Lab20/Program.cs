fe0621c5403b57d748d52a1f400d17befc751936
say-bay@mail.ru <say-bay@mail.ru>
2022-01-12 11:56:03 +0700

    Добавьте файлы проекта.

---
+++ Lab20/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab20
+{
+    class Program
+    {
+        delegate double MyDelegate(double R);
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate1 = Method1;
+            Console.WriteLine(myDelegate1(10));
+            MyDelegate myDelegate2 = Method2;
+            Console.WriteLine(myDelegate2(10));
+            MyDelegate myDelegate3 = Method3;
+            Console.WriteLine(myDelegate3(10));
+            Console.ReadKey();
+        }
+        static double Method1(double R)
+        {
+            return 2 * Math.PI * R;
+        }
+        static double Method2(double R)
+        {
+            return 2 * Math.PI * R * R;
+        }
+        static double Method3(double R)
+        {
+            return (4 / 3) * Math.PI * R * R * R;
+        }
+    }
+}
